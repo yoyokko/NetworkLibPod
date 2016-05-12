62a2997a33fd28b5ef34e6ed5cb59ec680bb3df8
Edward Chen <chenqinghua@qiyi.com>
2016-05-12 14:34:44 +0800

    add library and header for version 0.0.1

---
+++ NetworkLib.h
@@ -0,0 +1,17 @@
+//
+//  NetworkLib.h
+//  NetworkLib
+//
+//  Created by Edward Chen on 5/12/16.
+//  Copyright © 2016 Edward Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AFNetworking.h"
+
+@interface NetworkLib : NSObject
+
+- (void)getGithubReposForUser:(NSString*)user withSuccess:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
+
+@end
